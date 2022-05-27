ae205e6bb7280575d22f54f2813873507b0a2d1b
ShadowVMX <92258683+ShadowVMX@users.noreply.github.com>
2022-05-27 10:05:41 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+if [[ $EUID != 0 ]] ; then
+	echo " "
+	echo "$(tput setaf 6)[$(tput setaf 1)+$(tput setaf 6)]Solo ejecutar como root."
+	echo " "
+	exit
+fi
+
+apt update -y 
+apt full-upgrade -y
+apt install curl -y 
+apt install perl -y 
+apt install python3 -y
+apt install php -y 
+apt install cl-base64 -y
+
+clear
+echo " "
+echo "$(tput setaf 3)[$(tput setaf 1)*$(tput setaf 3)]Instalación exitosa."
+echo "" 
